7fd4012a0095a442c98c47b204b27a71cf9ed3c2
Waverly Sonntag <57609081+reneeverly@users.noreply.github.com>
2024-11-15 17:50:23 -0600

    Added additional window style; fixed bug with scpld windows.

--- misc/aframe/components/window.js
+++ misc/aframe/components/window.js
@@ -31,7 +31,15 @@ AFRAME.registerComponent('window', {
 		this.el.appendChild(newBoxGivenParams(bottom.join(' '), this.data.frame_width, depth, height))
 		this.el.appendChild(newBoxGivenParams([bottom[0] + width - this.data.frame_width, bottom[1], bottom[2]].join(' '), this.data.frame_width, depth, height))
 
-		if (this.data.style == 'scpld_single') {
+		if (this.data.style == 'simple_hung') {
+			// ┌─────┐
+			// ├─────┤
+			// └─────┘
+			
+			// one across
+			this.el.appendChild(newBoxGivenParams([bottom[0], bottom[1] + height * 2 - this.data.frame_width / 2, bottom[2]].join(' '), width, depth, this.data.frame_width))
+			
+		} else if (this.data.style == 'scpld_single') {
 			// ┌─────┐
 			// ├─┬─┬─┤
 			// │ │ │ │
@@ -40,15 +48,15 @@ AFRAME.registerComponent('window', {
 			
 			// horizontal crosses
 			// Bottom 2/9ths up
-			this.el.appendChild(newBoxGivenParams('cad-position', [bottom[0], bottom[1] + height * 2/9 - this.data.frame_width / 2, bottom[2]].join(' '), width, depth, this.data.frame_width))
+			this.el.appendChild(newBoxGivenParams([bottom[0], bottom[1] + height * 2/9 - this.data.frame_width / 2, bottom[2]].join(' '), width, depth, this.data.frame_width))
 			// Top 7/9ths up
-			this.el.appendChild(newBoxGivenParams('cad-position', [bottom[0], bottom[1] + height * 7/9 - this.data.frame_width / 2, bottom[2]].join(' '), width, depth, this.data.frame_width))
+			this.el.appendChild(newBoxGivenParams([bottom[0], bottom[1] + height * 7/9 - this.data.frame_width / 2, bottom[2]].join(' '), width, depth, this.data.frame_width))
 
 			// vertical crosses
 			// one third across
-			this.el.appendChild(newBoxGivenParams('cad-position', [bottom[0] + width / 3 - this.data.frame_width / 2, bottom[1] + height * 2/9, bottom[2]].join(' '), this.data.frame_width, depth, height * 5/9))
+			this.el.appendChild(newBoxGivenParams([bottom[0] + width / 3 - this.data.frame_width / 2, bottom[1] + height * 2/9, bottom[2]].join(' '), this.data.frame_width, depth, height * 5/9))
 			// two thirds across
-			this.el.appendChild(newBoxGivenParams('cad-position', [bottom[0] + width * 2 / 3 - this.data.frame_width / 2, bottom[1] + height * 2/9, bottom[2]].join(' '), this.data.frame_width, depth, height * 5/9))
+			this.el.appendChild(newBoxGivenParams([bottom[0] + width * 2 / 3 - this.data.frame_width / 2, bottom[1] + height * 2/9, bottom[2]].join(' '), this.data.frame_width, depth, height * 5/9))
 
 		} else if (this.data.style == 'scpld_triple') {
 			// ┌───┬───┬───┐
@@ -59,23 +67,23 @@ AFRAME.registerComponent('window', {
 
 			// horizontal crosses
 			// Bottom 2/9ths up
-			this.el.appendChild(newBoxGivenParams('cad-position', [bottom[0], bottom[1] + height * 2/9 - this.data.frame_width / 2, bottom[2]].join(' '), width, depth, this.data.frame_width))
+			this.el.appendChild(newBoxGivenParams([bottom[0], bottom[1] + height * 2/9 - this.data.frame_width / 2, bottom[2]].join(' '), width, depth, this.data.frame_width))
 			// Top 7/9ths up
-			this.el.appendChild(newBoxGivenParams('cad-position', [bottom[0], bottom[1] + height * 7/9 - this.data.frame_width / 2, bottom[2]].join(' '), width, depth, this.data.frame_width))
+			this.el.appendChild(newBoxGivenParams([bottom[0], bottom[1] + height * 7/9 - this.data.frame_width / 2, bottom[2]].join(' '), width, depth, this.data.frame_width))
 
 			// main verticals
 			// one third over
-			this.el.appendChild(newBoxGivenParams('cad-position', [bottom[0] + width / 3 - this.data.frame_width / 2, bottom[1], bottom[2]].join(' '), this.data.frame_width, depth, height))
+			this.el.appendChild(newBoxGivenParams([bottom[0] + width / 3 - this.data.frame_width / 2, bottom[1], bottom[2]].join(' '), this.data.frame_width, depth, height))
 			// two thirds over
-			this.el.appendChild(newBoxGivenParams('cad-position', [bottom[0] + width * 2 / 3 - this.data.frame_width / 2, bottom[1], bottom[2]].join(' '), this.data.frame_width, depth, height))
+			this.el.appendChild(newBoxGivenParams([bottom[0] + width * 2 / 3 - this.data.frame_width / 2, bottom[1], bottom[2]].join(' '), this.data.frame_width, depth, height))
 
 			// sub verticals
 			// one sixth over (halfway through first third)
-			this.el.appendChild(newBoxGivenParams('cad-position', [bottom[0] + width * 1 / 6 - this.data.frame_width / 2, bottom[1] + height * 2/9, bottom[2]].join(' '), this.data.frame_width, depth, height * 5/9))
+			this.el.appendChild(newBoxGivenParams([bottom[0] + width * 1 / 6 - this.data.frame_width / 2, bottom[1] + height * 2/9, bottom[2]].join(' '), this.data.frame_width, depth, height * 5/9))
 			// three sixths over (halfway through middle third)
-			this.el.appendChild(newBoxGivenParams('cad-position', [bottom[0] + width * 3 / 6 - this.data.frame_width / 2, bottom[1] + height * 2/9, bottom[2]].join(' '), this.data.frame_width, depth, height * 5/9))
+			this.el.appendChild(newBoxGivenParams([bottom[0] + width * 3 / 6 - this.data.frame_width / 2, bottom[1] + height * 2/9, bottom[2]].join(' '), this.data.frame_width, depth, height * 5/9))
 			// five sixths over (halfway through last third
-			this.el.appendChild(newBoxGivenParams('cad-position', [bottom[0] + width * 5 / 6 - this.data.frame_width / 2, bottom[1] + height * 2/9, bottom[2]].join(' '), this.data.frame_width, depth, height * 5/9))
+			this.el.appendChild(newBoxGivenParams([bottom[0] + width * 5 / 6 - this.data.frame_width / 2, bottom[1] + height * 2/9, bottom[2]].join(' '), this.data.frame_width, depth, height * 5/9))
 		}
 
 		// pane
